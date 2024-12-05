0f9239d59ee779c3b831e77d74f373979fd42ad0
Kelly Severson <kseverson26@student.cvtc.edu>
2024-12-05 16:44:44 -0600

    Correction to playerMoney.js

--- scripts/playerMoney.js
+++ scripts/playerMoney.js
@@ -5,5 +5,5 @@ function setPlayerMoney(bank) {
 
 function getPlayerMoney() {
   let bank = parseInt(localStorage.getItem('playerMoney'), 10);
-  return isNaN(bank) ? 100 : money; // Default to $100 if no valid money is found
+  return isNaN(bank) ? 100 : bank; // Default to $100 if no valid money is found
 }
